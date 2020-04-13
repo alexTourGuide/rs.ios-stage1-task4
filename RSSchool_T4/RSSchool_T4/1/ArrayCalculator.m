6e244b2a813c8bb2e0282ee20301a8c5108b0b5c
Alexander Porshnev <porshnev.alexander@gmail.com>
2020-04-13 19:12:25 +0300

    feat: implement 1st task

--- RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
+++ RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
@@ -2,7 +2,53 @@
 
 @implementation ArrayCalculator
 + (NSInteger)maxProductOf:(NSInteger)numberOfItems itemsFromArray:(NSArray *)array {
-    // good luck
-    return -1;
+    
+    NSMutableArray *mutableArray = [[NSMutableArray alloc] initWithArray:array];
+    for (id value in array) {
+        if (![value isKindOfClass:[NSNumber class]]) {
+            [mutableArray removeObject:value];
+        }
+    }
+    
+    if ([mutableArray count] == 0){
+        return 0;
+    } else if (numberOfItems > [mutableArray count]) {
+        numberOfItems = [mutableArray count];
+    }
+   
+    NSMutableArray *arrayCapacityMax = [NSMutableArray arrayWithCapacity:numberOfItems];
+    for (int i = 0; i < numberOfItems; i++) {
+        NSNumber *max = [mutableArray valueForKeyPath:@"@max.self"];
+        NSNumber *min = [mutableArray valueForKeyPath:@"@min.self"];
+        if (abs(max.intValue) > abs(min.intValue)) {
+            [arrayCapacityMax addObject:max];
+            [mutableArray removeObject:max];
+        } else if (abs(max.intValue) < abs(min.intValue)) {
+            [arrayCapacityMax addObject:min];
+            [mutableArray removeObject:min];
+            NSNumber *min2 = [mutableArray valueForKeyPath:@"@min.self"];
+            NSNumber *max2 = [mutableArray valueForKeyPath:@"@max.self"];
+            if (abs(max2.intValue) < abs(min2.intValue)) {
+                [arrayCapacityMax addObject:min2];
+                [mutableArray removeObject:min2];
+                i++;
+            } else {
+                [arrayCapacityMax removeObject:min];
+                [mutableArray addObject:min];
+                [arrayCapacityMax addObject:max];
+                [mutableArray removeObject:max];
+            }
+        } else {
+            [arrayCapacityMax addObject:max];
+            [mutableArray removeObject:max];
+        }
+    }
+    
+    int result = 1;
+    for (int i = 0; i < [arrayCapacityMax count]; i++) {
+        result *= [[arrayCapacityMax objectAtIndex:i] intValue];
+    }
+    
+    return result;
 }
 @end
